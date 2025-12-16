b2cb5904a2ac3ef0eeea2c2194015b450f71c246
MitaliLaddha <mitaliladdha2@gmail.com>
2025-12-16 22:47:43 +0530

    add command tokenization to build argv array for exec

--- src/shell.c
+++ src/shell.c
@@ -2,32 +2,42 @@
 #include <string.h>
 
 #define MAX_LINE 1024
+#define MAX_ARGS 64
 
 int main() {
     char line[MAX_LINE];
+    char *args[MAX_ARGS];
 
     while (1) {
-        // Print prompt
         printf("myshell> ");
         fflush(stdout);
 
-        // Read input
         if (fgets(line, MAX_LINE, stdin) == NULL) {
-            // Ctrl+D (EOF)
             printf("\n");
             break;
         }
 
-        // Remove trailing newline
         line[strcspn(line, "\n")] = '\0';
 
-        // Exit condition
         if (strcmp(line, "exit") == 0) {
             break;
         }
 
-        // echo input
-        printf("You typed: %s\n", line);
+        // Tokenize input
+        int argc = 0;
+        char *token = strtok(line, " ");
+
+        while (token != NULL && argc < MAX_ARGS - 1) {
+            args[argc++] = token;
+            token = strtok(NULL, " ");
+        }
+
+        args[argc] = NULL;  // VERY IMPORTANT
+
+        // Debug: print tokens
+        for (int i = 0; args[i] != NULL; i++) {
+            printf("arg[%d] = %s\n", i, args[i]);
+        }
     }
 
     return 0;
